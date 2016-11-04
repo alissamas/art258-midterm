72bb2a80cd8152298a67d7d8f274b70de14c3454
alissamas <aamasutani@gmail.com>
2016-11-03 16:20:07 -1000

    added mobile toggle

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,13 @@
-$(function() {
-    $("img.lazy").lazyload();
+var $navToggle = $('.nav-toggle');
+var $mainNavUl = $('.mobile-toggle');
+
+$navToggle.on('click', function() {
+  $mainNavUl.slideToggle();
+  $navToggle.toggleClass('active');
+});
+
+var $faqToggle = $('.nav-toggle');
+$faqToggle.on('click', function() {
+  $('.mobile-toggle').slideUp();
+  $(this).siblings().slideToggle();
 });
