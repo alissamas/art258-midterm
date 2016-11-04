7e117d0804fc77c9645d586b42e38719e8d018f9
alissamas <aamasutani@gmail.com>
2016-11-03 16:54:40 -1000

    mobile menu fixes

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,16 @@
 var $navToggle = $('.nav-toggle');
 var $mainNavUl = $('.mobile-toggle');
+var $window = $(window);
+var mobileBreakpoint = 750;
 
 $navToggle.on('click', function() {
   $mainNavUl.slideToggle();
   $navToggle.toggleClass('active');
 });
 
-var $faqToggle = $('.nav-toggle');
-$faqToggle.on('click', function() {
-  $('.mobile-toggle').slideUp();
-  $(this).siblings().slideToggle();
+$window.on('resize', function() {
+  if ($window.width() > mobileBreakpoint) {
+    $mainNavUl.show();
+    $navToggle.addClass('active');
+  }
 });
