8074458b88eb72c4996f12b4751b60d58b5f1835
alissamas <aamasutani@gmail.com>
2016-12-08 08:23:40 -1000

    add all my files

--- src/scripts.js
+++ src/scripts.js
@@ -2,38 +2,32 @@
 var $navToggle = $('.nav-toggle');
 var $mainNavUl = $('.mobile-toggle');
 var $window = $(window);
-var mobileBreakpoint = 750;
+var mobileBreakpoint = 780;
 
 $navToggle.on('click', function() {
   $mainNavUl.slideToggle();
   $navToggle.toggleClass('active');
+  event.preventDefault();
 });
 
 $window.on('resize', function() {
   if ($window.width() > mobileBreakpoint) {
     $mainNavUl.show();
     $navToggle.addClass('active');
+    event.preventDefault();
   }
 });
 
 // leaf movement
-$(".leaf").mouseover(function(){
+$(".leaf1 .leaf2").mouseover(function(){
     $(".leaf").css("transform", "rotate(3deg)");
 });
 
-$(".leaf").mouseout(function(){
+$(".leaf1, .leaf2").mouseout(function(){
     $(".leaf").css("transform", "rotate(-3deg)");
 });
 
-$(".leaf-2").mouseover(function(){
-    $(".leaf").css("transform", "rotate(3deg)");
-});
-
-$(".leaf-2").mouseout(function(){
-    $(".leaf").css("transform", "rotate(-3deg)");
-});
-
-// past events load more
+// past events
 $(document).ready(function () {
     $('.past-event-gallery li:lt(6)').show();
     $('.less').hide();
@@ -50,16 +44,19 @@ $(document).ready(function () {
 });
 
 // ScrollTrigger
-window.counter = function() {
+$(document).ready(function () {
+  window.counter = function() {
   // this refers to the html element with the data-scroll-showCallback tag
-  var span = this.querySelector('span');
-  var current = parseInt(span.textContent);
+    var span = this.querySelector('span');
+    var current = parseInt(span.textContent);
 
-  span.textContent = current + 1;
-};
+    span.textContent = current + 1;
+    event.preventDefault();
+  };
 
-document.addEventListener('DOMContentLoaded', function(){
-  var trigger = new ScrollTrigger({
-    addHeight: true
+  document.addEventListener('DOMContentLoaded', function(){
+    var trigger = new ScrollTrigger({
+      addHeight: true
+    });
   });
 });
